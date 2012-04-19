69b4af0a95e9636fb0daf91ed279de8833bee6e3
Gustavo Machado Campagnani Gama <gama@igenesis.com.br>
2012-04-19 15:23:04 -0300

    Don't display 'toggle reverse domains' button on dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class DashboardController < ApplicationController
     def index
-        @latest_domains = Domain.order('created_at DESC').limit(5)
+        @latest_domains = Domain.nonreverse.order('created_at DESC').limit(5)
     end
 end
