7ff13995ccef57ccc763d6bf1dd6f0a2d07b6f0b
Gustavo Machado Campagnani Gama <gama@igenesis.com.br>
2012-07-02 19:38:56 -0300

    Use "User.login" (instead of "id") on audit/syslog messages

--- app/controllers/audits_controller.rb
+++ app/controllers/audits_controller.rb
@@ -5,7 +5,7 @@ class AuditsController < ApplicationController
     before_filter :admin?
 
     def index
-        @audits = Audited::Adapters::ActiveRecord::Audit.reorder('id DESC').limit(20)
+        @audits = Audited::Adapters::ActiveRecord::Audit.includes(:user).reorder('id DESC').limit(20)
         @audits = @audits.paginate(:page => params[:page] || 1, :per_page => 20) if request.format.html? || request.format.js?
         respond_with(@audits) do |format|
             format.html { render :partial => 'list', :object => @audits, :as => :audits if request.xhr? }
--- app/helpers/syslog_helper.rb
+++ app/helpers/syslog_helper.rb
@@ -3,10 +3,10 @@ module SyslogHelper
         msg  = '[GloboDns]'
         msg << "[#{audit.auditable_type.downcase}:#{audit.action}:#{audit.auditable_id}]"
         msg << "[#{audit.associated_type.downcase}:#{audit.associated_id}]" if audit.associated_type
-        msg << "[user:#{audit.user_id}]"                                    if audit.user_id
+        msg << "[user:#{audit.user.login}]"                                 if audit.user && audit.user.login
         msg << "[from:#{audit.remote_address}]"                             if audit.remote_address
         msg << " #{audit.audited_changes.to_json}"
-        msg << " (#{audit.comment})"                               if audit.comment
+        msg << " (#{audit.comment})"                                        if audit.comment
 
         Rails.syslogger.info msg
     end
