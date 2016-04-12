a3965ff8865c56c951aa4968f5d325ce1336bf28
Ana Carolina Castro <anacarolinacastro@aol.com>
2016-04-12 17:05:58 -0300

    inicializa o logger

--- config/environment.rb
+++ config/environment.rb
@@ -18,5 +18,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 GloboDns::Application.initialize!
-Rails.logger = Logger.new(STDOUT)
-# Rails.logger = Log4r::Logger.new("Application Log")
