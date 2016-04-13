0c163de0313780655609b21fa92fa57d11ea5240
Ana Carolina Castro <anacarolinacastro@aol.com>
2016-04-13 14:44:18 -0300

    descomentar logger

--- app/controllers/bind9_controller.rb
+++ app/controllers/bind9_controller.rb
@@ -115,9 +115,9 @@ class Bind9Controller < ApplicationController
         end
 
     rescue Exception => e
-        logger.error "[ERROR] export failed: #{e}\n#{exporter.logger.string}\nbacktrace:\n#{e.backtrace.join("\n")}"
-        # logger.error "[ERROR] export failed"
-        # logger.error "#{e}"
+        # logger.error "[ERROR] export failed: #{e}\n#{exporter.logger.string}\nbacktrace:\n#{e.backtrace.join("\n")}"
+        logger.error "[ERROR] export failed"
+        logger.error "#{e}"
         # logger.error "#{exporter.logger.string}"
         # logger.error "backtrace: #{e.backtrace}"#.join("\n")}"
         [ e.to_s, :unprocessable_entity ]
