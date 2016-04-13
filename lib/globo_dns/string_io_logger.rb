fa7c03f547fcd3bda68f9e4be14e042cb33b7d01
Ana Carolina Castro <anacarolinacastro@aol.com>
2016-04-13 14:55:56 -0300

    Tentando arrumar o erro 'superclass must be a Class (Module given)'

--- lib/globo_dns/string_io_logger.rb
+++ lib/globo_dns/string_io_logger.rb
@@ -15,7 +15,8 @@
 
 module GloboDns
 
-class StringIOLogger < ActiveSupport
+class StringIOLogger 
+    extend ActiveSupport::TaggedLogging
 # class StringIOLogger < ActiveSupport::TaggedLogging
   
     def initialize(logger)
