415a199429107df2389251d93773b401d824d4bb
Ana Carolina Castro <anacarolinacastro@aol.com>
2016-04-26 16:05:37 -0300

    tentando arrumar o logger

--- lib/globo_dns/exporter.rb
+++ lib/globo_dns/exporter.rb
@@ -48,7 +48,7 @@ class Exporter
 
     def initialize
         # @logger = ActiveSupport::TaggedLogging.new(Rails.logger)
-        @logger = GloboDns::StringIOLogger.new(Logger.new(STDOUT))
+        @logger = GloboDns::StringIOLogger.new(Rails.Logger)
         @logger.loggers
         # @logger = GloboDns::StringIOLogger.new
         # @logger = Logger.new(STDOUT)
