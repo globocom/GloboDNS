fface0fae7816ba42d7979d843751fa1c89328e7
Ana Carolina Castro <anacarolinacastro@aol.com>
2016-04-27 17:07:49 -0300

    arrumando configuração de assets

--- config/application.rb
+++ config/application.rb
@@ -68,8 +68,8 @@ module GloboDns
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    app.config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')  
-    app.config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')  
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
 
     # application version
     version_file_name = File.expand_path('../../REVISION', __FILE__)
