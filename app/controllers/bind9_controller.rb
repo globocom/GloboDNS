85f48aeb0669b3c1574ebe0d705ab1c789a9f125
Ana Carolina Castro <anacarolinacastro@aol.com>
2016-04-28 13:51:27 -0300

    teste do que o  @output imprime

--- app/controllers/bind9_controller.rb
+++ app/controllers/bind9_controller.rb
@@ -108,6 +108,7 @@ class Bind9Controller < ApplicationController
             # sleep 60   # Keep this commented. Only for tests
             exporter.export_all(params['master-named-conf'], params['slave-named-conf'], :all => params['all'] == 'true', :keep_tmp_dir => true, :reset_repository_on_failure => true)
             # [ exporter.logger, :ok ]
+            [ "teste", :ok ]
         ensure
             Schedule.run_exclusive :export do |s|
                 s.date = nil
