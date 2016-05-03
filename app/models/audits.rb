837daac1253d4049239b51e1419d285a70b67a43
Ana Carolina Castro <anacarolinacastro@aol.com>
2016-05-03 15:20:37 -0300

    criado o model audits.rb

---
+++ app/models/audits.rb
@@ -0,0 +1,15 @@
+class Audits < ActiveRecord::Base
+    include SyslogHelper
+    include BindTimeFormatHelper
+
+    # scopes
+    default_scope             { order("#{self.table_name}.name") }
+    # scope :updated_since,     -> (timestamp) {Domain.where("#{self.table_name}.updated_at > ? OR #{self.table_name}.id IN (?)", timestamp, Record.updated_since(timestamp).select(:domain_id).pluck(:domain_id).uniq) }
+    scope :matching,			->(action) {Audited::Adapters::ActiveRecord::Audit.where("action = ?", action)}
+    # scope :matching,          -> (userQuery){
+    #                                 if userQuery.index('*')
+    #                                     where("#{self.table_name}.name LIKE ?", userQuery.gsub(/\*/, '%'))
+    #                                 else
+    #                                     where("#{self.table_name}.name" => userQuery)
+    #                                 end
+                                }
