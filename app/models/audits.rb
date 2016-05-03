61c7a2e57efbce48e0c3cb4582056c7b33fbe1b7
Ana Carolina Castro <anacarolinacastro@aol.com>
2016-05-03 17:24:32 -0300

    model audits

--- app/models/audits.rb
+++ app/models/audits.rb
@@ -2,6 +2,8 @@ class Audits < ActiveRecord::Base
     include SyslogHelper
     include BindTimeFormatHelper
 
+    attr_accessible :time, :user_id, :from, :action, :entity, :association, :changes;
+
     # scopes
     default_scope             { order("#{self.table_name}.name") }
     # scope :updated_since,     -> (timestamp) {Domain.where("#{self.table_name}.updated_at > ? OR #{self.table_name}.id IN (?)", timestamp, Record.updated_since(timestamp).select(:domain_id).pluck(:domain_id).uniq) }
