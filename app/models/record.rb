c0c80fcd30e685ab38214eab44d4a12abffcf786
Ana Carolina Castro <anacarolinacastro@aol.com>
2016-05-04 17:29:09 -0300

    erro quando nome do record já existir no domain

--- app/models/record.rb
+++ app/models/record.rb
@@ -217,18 +217,19 @@ class Record < ActiveRecord::Base
     end
 
     def validate_name_unique
-        unless self.name != '@'
-            names = []
-            domain = Domain.where(id: :domain_id).first
-            for n in domain.records
-                names.append(n)
-            end
-            
-            if names.include?(self.name)
-                puts "teste"
-                self.errors.add(:name, I18n.t('invalid', :scope => 'activerecord.errors.messages'))
-            end
+        return if self.name.blank? || self.name == '@'
+
+        names = []
+        domain = Domain.where(id: self.domain_id).first
+        for n in domain.records
+            names.append(n)
         end
+        
+        if names.include?(self.name)
+            puts "teste"
+            self.errors.add(:name, I18n.t('invalid', :scope => 'activerecord.errors.messages'))
+        end
+
         # if self.class.where('id != ?', self.id).where('name' => self.name).first != nill
         # if Domain.where(id: :domain_id).first.records.where(name: :name).first != nil
         # if self.class.where('id != ?', self.id).where('name' => self.name, 'domain_id' => self.domain_id).first != nil
