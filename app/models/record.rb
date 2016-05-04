5d06dff210f426a26389398c6e34220c09c20389
Ana Carolina Castro <anacarolinacastro@aol.com>
2016-05-04 16:54:17 -0300

    erro quando nome do record já existir no domain

--- app/models/record.rb
+++ app/models/record.rb
@@ -228,6 +228,10 @@ class Record < ActiveRecord::Base
         #         self.errors.add(:name, I18n.t('invalid', :scope => 'activerecord.errors.messages'))
         #     end
         # end
+        # if self.class.where('id != ?', self.id).where('name' => self.name).first != nill
+        if Domain.where(id: :domain_id).first.records.where(name: :name).first
+            self.errors.add(:name, I18n.t('recursive_subdomain', :domain => domain.name, :scope => 'activerecord.errors.messages'))
+        end
     end
 
     def validate_name_format
