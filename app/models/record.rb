ef30299c7496bc322b8d24fa83a200cfaf8192c7
Ana Carolina Castro <anacarolinacastro@aol.com>
2016-05-05 10:19:18 -0300

    records com o mesmo nome, typo e content não deve ser permitido

--- app/models/record.rb
+++ app/models/record.rb
@@ -41,9 +41,9 @@ class Record < ActiveRecord::Base
     validate                   :validate_same_name_and_type_and_content,    :unless => :importing?
 
     # validations that generate 'warnings' (i.e., doesn't prevent 'saving' the record)
-    # validation_scope :warnings do |scope|
-    #     scope.validate :validate_same_name_and_type
-    # end
+    validation_scope :warnings do |scope|
+        scope.validate :validate_same_name_and_type
+    end
 
     class_attribute :batch_soa_updates
 
@@ -268,7 +268,7 @@ class Record < ActiveRecord::Base
 
     def validate_same_name_and_type_and_content
         if record = self.class.where('id != ?', self.id).where('name' => self.name, 'type' => self.type, 'domain_id' => self.domain_id, 'content' => self.content).first
-            self.errors.add(:name, I18n.t('record_same_name_and_type', :name => record.name, :type => record.type, :content => record.content, :scope => 'activerecord.errors.messages'))
+            self.errors.add(:name, I18n.t('record_same_name_and_type', :scope => 'activerecord.errors.messages'))
             return
         end
     end
