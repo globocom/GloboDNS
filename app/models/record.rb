7e1e850b6bef8f52b1b51dce893b769509318281
Ana Carolina Castro <anacarolinacastro@aol.com>
2016-05-05 11:20:53 -0300

    records com o mesmo nome, typo e content não deve ser permitido

--- app/models/record.rb
+++ app/models/record.rb
@@ -220,10 +220,10 @@ class Record < ActiveRecord::Base
     def validate_name_cname
         if self.type == "CNAME"
             # if record = self.class.where('id != ?', self.id).where('name' => self.name, 'domain_id' => self.domain_id).first
-            if record = self.class.where('id != ?', self.id).where('name' => self.name, 'type' => "A", 'domain_id' => self.domain_id).first
-                self.errors.add(:name, "deve ser único para o tipo CNAME")
-                return
-            end
+            # if record = self.class.where('id != ?', self.id).where('name' => self.name, 'type' => "A", 'domain_id' => self.domain_id).first
+            self.errors.add(:name, "deve ser único para o tipo CNAME")
+            return
+            # end
         end
     end
 
