ab9daca39237e3425ef2fd12851069377f7886a0
Ana Carolina Castro <anacarolinacastro@aol.com>
2016-05-05 16:46:28 -0300

    records com o mesmo nome, typo e content não deve ser permitido

--- app/models/record.rb
+++ app/models/record.rb
@@ -234,7 +234,7 @@ class Record < ActiveRecord::Base
 
         self.name.split('.').each_with_index do |part, index|
             if self.type == "SRV"
-                unless (index == 0 && part == '*') || part =~ /^(?![0-9]+$)(?!-)[a-zA-Z0-9-_]{,63}(?<!-)$/
+                unless (index == 0 && part == '*') || part =~ /^(?![0-9]+$)(?!-)[a-zA-Z0-9\-_]{,63}(?<!-)$/
                     self.errors.add(:name, I18n.t('invalid', :scope => 'activerecord.errors.messages'))
                     return
                 end
