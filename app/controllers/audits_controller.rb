274ec11e559a8f3cb7934b270b67d5de691dd941
Ana Carolina Castro <anacarolinacastro@aol.com>
2016-07-05 16:24:26 -0300

    Prevent export to delete zones already removed

--- app/controllers/audits_controller.rb
+++ app/controllers/audits_controller.rb
@@ -22,6 +22,7 @@ class AuditsController < ApplicationController
     def index
         @audits = Audited::Adapters::ActiveRecord::Audit.includes(:user).reorder('id DESC').limit(20)
         @audits = @audits.paginate(:page => params[:page] || 1, :per_page => 20) if request.format.html? || request.format.js?
+        @audits = @audits.matching(params[:query]) if params[:query].present?
         respond_with(@audits) do |format|
             format.html { render :partial => 'list', :object => @audits, :as => :audits if request.xhr? }
         end
