acbbe9021c46034cdc7d7aa29986be694597e57c
Ana Carolina Castro <anacarolinacastro@aol.com>
2016-09-28 11:02:26 -0300

    Adding logs filtering options

--- app/controllers/audits_controller.rb
+++ app/controllers/audits_controller.rb
@@ -20,9 +20,65 @@ class AuditsController < ApplicationController
     before_filter :admin_or_operator?
 
     def index
-        @audits = Audited::Adapters::ActiveRecord::Audit.includes(:user).reorder('id DESC').limit(20)
-        @audits = @audits.paginate(:page => params[:page] || 1, :per_page => 20) if request.format.html? || request.format.js?
-        @audits = @audits.matching(params[:query]) if params[:query].present?
+        @audits = Audited::Adapters::ActiveRecord::Audit.includes(:user)
+
+        # filtros dos logs
+        if params[:audit_action] 
+            @audits = @audits.where(action: params[:audit_action]) unless params[:audit_action] == "all"
+        end
+        if params[:audit_user] && params[:audit_user]!=""
+            @audits = @audits.where(user: User.find(params[:audit_user]))
+        end
+        if params[:audit_record] && params[:audit_record]!=""
+            ids = []
+            @audits.where(auditable_type: "Record").each do |a|
+                ids.push(a.id) if a['audited_changes']['name'] == params[:audit_record]
+            end
+            @audits = @audits.where({id: ids})
+        end
+        if params[:audit_record_type] && params[:audit_record_type]!=""
+            ids = []
+            @audits.where(auditable_type: "Record").each do |a|
+                ids.push(a.id) if a['audited_changes']['type'] == params[:audit_record_type]
+            end
+            @audits = @audits.where({id: ids})
+        end
+        if params[:audit_domain] && params[:audit_domain]!=""
+            ids = []
+            domain_id = 0
+            # audits do domain buscado
+            @audits.where(auditable_type: "Domain").each do |a|
+                ids.push(a.id) if a['audited_changes']['name'] == params[:audit_domain]
+            end
+            # audits de records do domain buscado
+            if domain = Domain.where(name: params[:audit_domain]).first
+                # o dominio buscado ainda existe no banco de dados
+                @audits.where(auditable_type: "Record").each do |a|
+                    ids.push(a.id) if a['audited_changes']['domain_id'] == domain.id
+                end
+            else
+                # procura a id do dominio nos logs dos domains deletados
+                deleted = Audited::Adapters::ActiveRecord::Audit.where(auditable_type: "Domain", action: "Destroy")
+                deleted.each do |d|
+                    if d['audited_changes']['name'] == params[:audit_domain]
+                        domain_id = d.auditable_id
+                        break
+                    end
+                end
+                # o dominio buscado foi deletado
+                if domain_id != 0
+                    @audits.where(auditable_type: "Record").each do |a|
+                        ids.push(a.id) if a['audited_changes']['domain_id'] == domain_id
+                    end
+                # dominio nunca existiu
+                else
+                end
+            end
+            @audits = @audits.where({id: ids})
+        end
+        # fim dos filtros
+
+        @audits = @audits.reorder('id DESC').paginate(:page => params[:page] || 1, :per_page => 20) if request.format.html? || request.format.js?
         respond_with(@audits) do |format|
             format.html { render :partial => 'list', :object => @audits, :as => :audits if request.xhr? }
         end
