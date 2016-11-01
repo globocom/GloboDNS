083f08fa278721196b0f78fc66379ae1bf1610c8
Daehyung Lee <daehyung@gmail.com>
2016-11-01 06:53:24 +0000

    updated deprecated method, deliver

--- lib/globo_dns/importer.rb
+++ lib/globo_dns/importer.rb
@@ -264,10 +264,10 @@ class Importer
         end
 
         syslog_info('import successful')
-        Notifier.import_successful(logger).deliver
+        Notifier.import_successful(logger).deliver_now
     rescue Exception => e
         syslog_error 'import failed'
-        Notifier.import_failed("#{e}\n\n#{logger}\n\nBacktrace:\n#{e.backtrace.join("\n")}").deliver
+        Notifier.import_failed("#{e}\n\n#{logger}\n\nBacktrace:\n#{e.backtrace.join("\n")}").deliver_now
         raise e
     # ensure
         # FileUtils.remove_entry_secure master_tmp_dir unless master_tmp_dir.nil? || @options[:keep_tmp_dir] == true
