3bbe74709e38dd833d009c99c708028828bc408a
Ana Carolina Castro <ana.castro@corp.globo.com>
2018-12-06 17:12:31 -0200

    Partials for ownership information

--- lib/domain_ownership.rb
+++ lib/domain_ownership.rb
@@ -7,27 +7,27 @@ module DomainOwnership
     def initialize
     end
 
-    def post_domain_ownership_info(name, component_id, sub_component_id)
+    def has_permission?(name, user, action = @permissions_action)
+      false
+    end
+
+    def users_permissions_info(user)
+      {:sub_components => [["sub_component_name_1", "sub_component_id_1"],["sub_component_name_n", "sub_component_id_n"]]}
+    end
+
+    def get_sub_components_from_user(user)
+      [["sub_component_name_1", "sub_component_id_1"],["sub_component_name_n", "sub_component_id_n"]]
+    end
+
+    # ARTEMIA
+    def patch_domain_ownership_info(id, name, sub_component, classifier)
+    end
+
+    def post_domain_ownership_info(name, sub_component_id, classifier, user)
     end
 
     def get_domain_ownership_info(name)
-      { component_id:  "id_component", sub_component_id: "id_sub_component" }
-    end
-
-    def get_components_ids
-      [["name1", "id1"],["nameN", "idN"]]
-    end
-
-    def get_sub_components_ids
-      [["name1", "id1"],["nameN", "idN"]]
-    end
-
-    def get_component_name(id)
-      "component name"
-    end
-
-    def get_sub_component_name(id)
-      "sub component name"
+      { sub_component: "sub_component", sub_component_id: "sub_component_id", id: "id" }
     end
   end
 end
