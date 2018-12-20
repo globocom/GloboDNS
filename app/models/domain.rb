09bf0916d091f0ff6592c945bf2ae97c903176b4
felipe.oliveira <felipe.oliveira@corp.globo.com>
2018-12-20 11:54:54 -0200

    Fix domain

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -177,8 +177,7 @@ class Domain < ActiveRecord::Base
 
   def set_ownership(sub_component, user)
     if GloboDns::Config::DOMAINS_OWNERSHIP
-      owner = DomainOwnership::API.instance.post_domain_ownership_info(self.name, sub_component, "domain", user) if DomainOwnership::API.instance.get_domain_ownership_info(self.name)[:sub_component].nil?
-      self.errors.add(:base, "User doesn't have ownership of '#{self.name}'") unless owner
+      DomainOwnership::API.instance.post_domain_ownership_info(self.name, sub_component, "domain", user) if DomainOwnership::API.instance.get_domain_ownership_info(self.name)[:sub_component].nil?
     end
   end
 
